d5803551d04eaebfed5396882baca2b363609274
artem-vildanov <tema2000art@gmail.com>
2024-06-05 23:50:37 +0700

    delete Boolean.ts

--- src/RPNGenerator/DataTypes/Boolean.ts
+++
@@ -1,9 +0,0 @@
-import DataType from "./DataType";
-
-export default class Boolean extends DataType {
-  type: string = "boolean";
-  value: boolean = true;
-  constructor(name: string) {
-    super(name);
-  }
-}
